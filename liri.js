ec96e3cb86c80b86ed4b4454f6649c23eadbdd4a
Legionnaire <blake.marter@gmail.com>
2019-04-26 19:30:04 -0700

    innitial files

---
+++ liri.js
@@ -0,0 +1,14 @@
+// LIRI Homework JS
+
+// require("dotenv").config();
+
+var spotify = require("node-spotify-api");
+var axios = require("axios");
+var moment = require("moment");
+
+// const db = require('db')
+// db.connect({
+//   host: process.env.DB_HOST,
+//   username: process.env.DB_USER,
+//   password: process.env.DB_PASS
+// })
